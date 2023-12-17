f3fbf914c8144d6673137805043011f58d5fc95c
Matthew Rawlings <mbrawlings11@gmail.com>
2023-12-17 13:51:43 -0700

    modal implemented

--- screens/Rounds.js
+++ screens/Rounds.js
@@ -1,6 +1,13 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Modal,
+  Pressable,
+  Dimensions,
+} from "react-native";
 import { TextInput, IconButton, Button } from "react-native-paper";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRoute } from "@react-navigation/native";
 
 export default function Rounds({ navigation }) {
@@ -14,6 +21,8 @@ export default function Rounds({ navigation }) {
     },
   };
   const [currentTurn, setCurrentTurn] = useState(route.params.players[0].name);
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
     <View style={styles.container}>
       <View
@@ -76,6 +85,68 @@ export default function Rounds({ navigation }) {
         }}
       >
         <View>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert("Modal has been closed.");
+              setModalVisible(!modalVisible);
+            }}
+          >
+            <View style={styles.centeredView}>
+              <View style={(styles.modalView)}>
+                <View style={styles.row}>
+                  <Button style={{ width: "20%" }} mode="contained">
+                    2
+                  </Button>
+                  <Button style={{ width: "20%" }} mode="contained">
+                    3
+                  </Button>
+                  <Button style={{ width: "20%" }} mode="contained">
+                    4
+                  </Button>
+                  <Button style={{ width: "20%" }} mode="contained">
+                    5
+                  </Button>
+                </View>
+                <View style={styles.row}>
+                  <Button style={{ width: "20%" }} mode="contained">
+                    6
+                  </Button>
+                  <Button style={{ width: "20%" }} mode="contained">
+                    7
+                  </Button>
+                  <Button style={{ width: "20%" }} mode="contained">
+                    8
+                  </Button>
+                  <Button style={{ width: "20%" }} mode="contained">
+                    9
+                  </Button>
+                </View>
+                <View style={styles.row}>
+                  <Button style={{ width: "20%" }} mode="contained">
+                    10
+                  </Button>
+                  <Button style={{ width: "20%" }} mode="contained">
+                    11
+                  </Button>
+                  <Button style={{ width: "20%" }} mode="contained">
+                    12
+                  </Button>
+                  <Button style={{ width: "20%" }} mode="contained">
+                    Doubles!
+                  </Button>
+                </View>
+                <Pressable
+                  style={[styles.button, styles.buttonClose]}
+                  onPress={() => setModalVisible(!modalVisible)}
+                >
+                  <Text>Hide Modal</Text>
+                </Pressable>
+              </View>
+            </View>
+          </Modal>
           {route?.params?.players?.map((player) => (
             <View
               style={{
@@ -103,7 +174,11 @@ export default function Rounds({ navigation }) {
               justifyContent: "space-around",
             }}
           >
-            <Button mode="contained" style={{ width: "40%" }}>
+            <Button
+              mode="contained"
+              style={{ width: "40%" }}
+              onPress={() => setModalVisible(true)}
+            >
               ROLL
             </Button>
             <Button mode="contained" style={{ width: "40%" }}>
@@ -121,5 +196,44 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "start",
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  modalView: {
+    height: "70%",
+    width: "100%",
+    padding: "6%",
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+    overflow: "hidden",
+    backgroundColor: "white",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  row: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "100%",
+    height: "20%",
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
   },
 });
